df1cd0f94107ae4ab9f0650d261aa9aca31153d4
ncskier <bcw58@cornell.edu>
2016-12-16 12:04:28 -0500

    created some walls, textures and walking scripts - I don't know if we have git working properly with Unity yet

---
+++ Assets/Custom/Scripts/AutoWalk.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoWalk : MonoBehaviour {
+
+	[Tooltip ("Speed at which the player walks")]
+	public float speed;
+
+	[Tooltip ("Acceleration when player starts or stops walking")]
+	public float acceleration;
+
+	[Tooltip ("Gvr Viewer in the scene")]
+	public GvrViewer viewer;
+
+	private Camera headCamera;
+	private bool walking;
+	private float currentSpeed;
+
+	// Use this for initialization
+	void Start () {
+		headCamera = this.GetComponentInChildren<Camera> ();
+		walking = false;
+	}
+
+	private void Walk (float speed) {
+		Vector3 xzTranslation = new Vector3 (headCamera.transform.forward.x, 0, headCamera.transform.forward.z);
+		this.transform.position += xzTranslation.normalized * speed * Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (viewer.Triggered) {
+			walking = !walking;
+		}
+
+		if (walking) {
+			if (currentSpeed < speed) {
+				currentSpeed += acceleration * Time.deltaTime;
+				if (currentSpeed > speed) {
+					currentSpeed = speed;
+				}
+			}
+			Walk (currentSpeed);
+		} else {
+			if (currentSpeed > 0) {
+				currentSpeed -= acceleration * Time.deltaTime;
+				if (currentSpeed < 0) {
+					currentSpeed = 0;
+				}
+				Walk (currentSpeed);
+			}
+		}
+
+	}
+}
